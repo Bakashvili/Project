eee74434b287a8a1473c7f7a69dd79c1d3bd146a
nato bakashvili <natobakashvili@icloud.com>
2023-12-26 22:16:36 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,80 @@
+const Sequelize = require('sequelize');
+const DataAccesLayer = require('./DataAccesLayer');
+const UserServices = require('./Api_Services/UserServices');
+const UserController = require('./Api_Controller/UserController');
+const { UserCredentials } = require('./Api_Contract/UserCotract');
+const User = DataAccesLayer.User;
+const Result= DataAccesLayer.Result;
+const Level = DataAccesLayer.Level;
+const Levelanswer = DataAccesLayer.Levelanswer;
+const Trainsession = DataAccesLayer.Trainsession;
+const DBcontext = DataAccesLayer.index;
+// function a(){
+//     User.findAll().then(result =>{
+//        console.log(result);
+//     })
+// }
+// a();
+// function a1(){
+//    Result.findAll().then(result_1 =>{
+//        console.log(result_1);
+//     })
+// }
+// a1();
+
+// // Добавить user_id и связи
+// function a2(){
+//    Level.findAll().then(result_2 =>{
+//        console.log(result_2);
+//     })
+// }
+// a2();
+// function a3(){
+//    Levelanswer.findAll().then(result_3 =>{
+//        console.log(result_3);
+//     })
+// }
+// a3();
+// function a4(){
+//    Trainsession.findAll().then(result_4 =>{
+//        console.log(result_4);
+//     })
+// }
+// a4();
+// (async () => {
+//     try {
+//       // Найти все записи в таблице Levelanswer и включить связанную модель User
+//       const levelAnswersWithUser = await Levelanswer.findAll({
+//         include: User
+//       });
+  
+//       console.log(JSON.stringify(levelAnswersWithUser, null, 2));
+//     } catch (error) {
+//       console.error('Error:', error);
+//     } finally {
+//       await Sequelize.close();
+//     }
+//   })();
+(async () => {
+    try {
+      // Найти пользователя с ID 1
+      const user = await User.findByPk(1);
+  
+      if (user) {
+        // Обновить данные в таблице Result для этого пользователя
+        const updatedResults = await Result.update(
+          { Score: 55, Times: 13 },
+          { where: { UserId: user.Id } }
+        );
+  
+        console.log(updatedResults); // Вывести количество обновленных записей
+      } else {
+        console.log('Пользователь с ID 1 не найден.');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    } finally {
+     
+    }
+  })();
+UserController.Register(UserCredentials('nato','pasword') );
